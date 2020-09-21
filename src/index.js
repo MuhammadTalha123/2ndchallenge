b36dbf626e93823185e11941471b5de61b88ffa0
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-09-22 00:08:56 +0500

    initial commite

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const myName = 'Talha Bajwa';
+
+var today = new Date();
+
+var date = today.toLocaleDateString();
+
+var time = today.toLocaleTimeString();
+
+ReactDom.render(
+  <>
+  <h1>{`My name is ${myName}.`}</h1>
+  <p>{`Curren Date Is ${date}`}</p>
+  <p>{`Curren Time Is ${time}`}</p>
+  </>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
